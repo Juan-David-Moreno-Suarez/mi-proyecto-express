6428be1799908020521bc02a76e29dc16694a8c2
Juan-David-Moreno-Suarez <juanmorsua@unisabana.edu.co>
2026-04-07 11:11:16 -0500

    Level 1

--- src/app.js
+++ src/app.js
@@ -10,6 +10,12 @@ app.get('/hola', (req, res) => {
     res.json({ mensaje: `¡Hola Mundo desde Express ${id}!` });
 });
 
+app.get('/toFahrenheit', (req, res) => {
+    const {celsius} = req.query;
+    const fahrenheit = celsius*9/5 + 32;
+    res.json({mensaje: `resultado: ${fahrenheit} °F`})
+});
+
 // Iniciar el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
